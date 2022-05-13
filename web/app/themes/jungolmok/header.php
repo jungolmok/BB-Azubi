a950f8cf00a3606fdfe736bd13b6fba408c20b7e
ko <ko@bundb.de>
2022-05-13 17:03:36 +0200

    update 1.2

--- web/app/themes/jungolmok/header.php
+++ web/app/themes/jungolmok/header.php
@@ -20,7 +20,14 @@
 		<div class='header--container'>
 			<div class="logo">
 				<a href="<?php echo esc_url(home_url('')); ?>" rel="home">
-				<figure class="icons"><img width="48" height="48" src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/logo--sample.svg" alt="logo"></figure>
+				<figure class="icons">
+					<img 
+						width="48" 
+						height="48" 
+						src="<?php echo get_template_directory_uri(); ?>/assets/images/svg/logo--sample.svg" 
+						alt="logo" 
+					/>
+				</figure>
 				</a>
 			</div>
 		</div>
